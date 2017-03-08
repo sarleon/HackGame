01c46703d72b92a926da87d4d528862077803142
sarleon <sarleon56@hotmail.com>
2017-03-08 23:48:37 +0800

    add tools

--- app/tools/views.py
+++ app/tools/views.py
@@ -3,3 +3,22 @@ from . import tools
 @tools.route('/')
 def index():
     return
+
+
+@tools.route('/wenku')
+def wenku():
+    return
+
+
+
+@tools.route('/hash')
+def hash():
+    return
+
+@tools.route('/crypt')
+def crypt():
+    return
+
+@tools.route('/encode')
+def encode():
+    return
